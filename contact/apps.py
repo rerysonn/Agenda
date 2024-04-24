6403acfacf6a2229f2ac562753f1ff9987fa64ed
Reryson Farinha <rerysonfarinha@outlook.com>
2024-04-24 15:13:30 -0300

    Criando APP contact

--- contact/apps.py
+++ contact/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AgendaConfig(AppConfig):
+class contactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agenda'
+    name = 'contact'
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from agenda.views import index
+from contact.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
