a380e75e965f4579a5b3130707ffba36c40d0228
Reryson Farinha <rerysonfarinha@outlook.com>
2024-04-25 10:17:49 -0300

    Adicionando campos no Admin

--- contact/models.py
+++ contact/models.py
@@ -3,8 +3,11 @@ from django.utils import timezone
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
     phone = models.CharField(max_length=50)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
