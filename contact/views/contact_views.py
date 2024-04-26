ad70a529f3a48b1dbb9997692bb04fc93703c787
Reryson Farinha <rerysonfarinha@outlook.com>
2024-04-26 19:07:50 -0300

    Implementando a funcao Search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.db.models import Q
 
 
 def index(request):
@@ -32,5 +33,33 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show = True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
+        )\
+        .order_by('id')
+
+    context = {
+        'contacts' : contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request, 
+        'contact/index.html',
         context
     )
