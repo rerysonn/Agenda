0d8c1d87deb587ddb99c5425e028e0e0e3baa477
Reryson Farinha <rerysonfarinha@outlook.com>
2024-04-26 14:15:11 -0300

    Implementando partials aos templates e alterando o tittle da agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -9,7 +9,8 @@ def index(request):
         .order_by('id')[0:10]
 
     context = {
-        'contacts' : contacts
+        'contacts' : contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -21,8 +22,11 @@ def index(request):
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show= True) 
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
-        'contact' : single_contact
+        'contact' : single_contact,
+        'site_title' : site_title
     }
 
     return render(
