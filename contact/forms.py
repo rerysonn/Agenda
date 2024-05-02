63d06510040b5bdf6b7dd31e48609fbebfe14cd1
Reryson Farinha <rerysonfarinha@outlook.com>
2024-05-02 16:41:29 -0300

    Validação de formulário

--- contact/forms.py
+++ contact/forms.py
@@ -41,12 +41,29 @@ class ContactForms(forms.ModelForm):
         )
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome deve ser diferente do segundo nome',
                 code='invalid'
             )
-        )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Erro',
+                    code='invalid'
+                )
+            )
+        return first_name
