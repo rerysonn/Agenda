1c8a891f790e882716b6bf17043e98e029f26998
Reryson Farinha <rerysonfarinha@outlook.com>
2024-05-02 15:20:54 -0300

    Organizando importações

---
+++ contact/forms.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ValidationError
+from django import forms
+from . import models
+
+class ContactForms(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone',
+        )
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        return super().clean()
