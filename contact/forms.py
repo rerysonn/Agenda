2a00da14f87c5f1d4cd43e64c8b32b32995c0379
Reryson Farinha <rerysonfarinha@outlook.com>
2024-05-07 11:03:04 -0300

    Adicionando campos ao forms de register

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ValidationError
 from django import forms
 from . import models
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class ContactForms(forms.ModelForm):
 
@@ -95,4 +97,39 @@ class ContactForms(forms.ModelForm):
         return first_name
     
 class RegisterForm(UserCreationForm):
-    ...
+
+    first_name = forms.CharField(
+        label= 'Primeiro nome',
+        required= True,
+        min_length=2
+    )
+    last_name = forms.CharField(
+        label= 'Ultimo nome',
+        required= True,
+        min_length=2
+    )
+    email = forms.EmailField(
+        required= True,
+        min_length=2
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2'
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('email existente', code='invalid')
+            )
+        
