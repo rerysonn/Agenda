eb3dd9dfb846a5a9d09aa43741552df06990ba56
Reryson Farinha <rerysonfarinha@outlook.com>
2024-05-07 11:22:01 -0300

    Usando messages para enviar mensagens

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
+from django.contrib import messages
+
+
 
 def register(request):
     form = RegisterForm()
+
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid(): 
             form.save()
+            messages.success(request, 'Usuário registrado com sucesso!')
+            return redirect('contact:index')
     
     return render(
         request,
